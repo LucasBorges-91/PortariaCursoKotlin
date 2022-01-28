15cd5e96f29053b59f1b133bfb1fd45a59bb3f45
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-28 17:20:31 -0300

    criando as classes console e portaria

---
+++ src/main/kotlin/Portaria.kt
@@ -0,0 +1,14 @@
+import java.nio.file.Files.readString
+
+class Portaria {
+
+    fun controle() {
+        val idade = Console.readInt( "Qual sua idade?" )
+        if ( idade < 18 ) {
+            println( "Negado. Menores de idade não são permitidos!" )
+            return
+        }
+
+        val tipoConvite = Console.readString( "Qual é o tipo de convite?" )
+    }
+}
